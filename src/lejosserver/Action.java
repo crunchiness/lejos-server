583254a654d6ff5a4acfb49dbc66bec0fd096c27
Ingvaras Merkys <ingvaras@gmail.com>
2015-06-23 14:54:23 +0100

    returns output through TCP

--- src/lejosserver/Action.java
+++
@@ -1,5 +0,0 @@
-package lejosserver;
-
-public interface Action {
-
-}
--- src/lejosserver/LocalServer.java
+++ src/lejosserver/LocalServer.java
@@ -13,29 +13,30 @@ public class LocalServer {
 	// Motor ports: A, B, C, D
 	private Motor[] motors = {null, null, null, null};
 	// Sensor ports: 1, 2, 3, 4
-	private Sensor[] sensors = {null, null, null, null};
+	//private Sensor[] sensors = {null, null, null, null};
 	
 	public LocalServer() {}
 	
 	public static void main(String argv[]) throws Exception {
 		LocalServer server = new LocalServer();
 		String clientSentence;
-		String capitalizedSentence;
 		ServerSocket welcomeSocket = new ServerSocket(6789);
-		LCD.drawString("OK", 0, 4);
+		LCD.drawString("CONGRATZ", 0, 4);
 		while (true) {
 			Socket connectionSocket = welcomeSocket.accept();
 			BufferedReader inFromClient = new BufferedReader(
 					new InputStreamReader(connectionSocket.getInputStream()));
+			clientSentence = inFromClient.readLine();
 			DataOutputStream outToClient = new DataOutputStream(
 					connectionSocket.getOutputStream());
-			clientSentence = inFromClient.readLine();
+			PrintWriter pw = new PrintWriter(outToClient);
+			
 			LCD.drawString("Received: " + clientSentence, 0, 4);
 			
-			JSONObject obj = (JSONObject) JSONValue.parse(clientSentence);
+			JSONObject inputObj = (JSONObject) JSONValue.parse(clientSentence);
 			JSONObject contents;
 			try {
-				contents = (JSONObject) obj.get("command");
+				contents = (JSONObject) inputObj.get("command");
 			} catch (NullPointerException e) {
 				LCD.drawString("Bad input: " + clientSentence, 0, 4);
 				break;
@@ -59,14 +60,19 @@ public class LocalServer {
 			// Motor commands
 			} else if (dev.equals(new String("motor"))) {
 				String port = (String) contents.get("port");
+				int portIndex = getPortIndex(port); //TODO handle
 				if (cmd.equals(new String("init"))) {
 					server.init_motor(port);
+				} else if (cmd.equals(new String("getspeed"))) {
+					int speed = server.motors[portIndex].getSpeed();
+					JSONObject outputObj = new JSONObject();
+					outputObj.put("speed", new Integer(speed));
+					StringWriter out = new StringWriter();
+					outputObj.writeJSONString(out);
+					String jsonOutput = out.toString();
+					pw.println(padString(jsonOutput));
+			        pw.flush();
 				} else {
-					int portIndex = getPortIndex(port);
-					if (portIndex == -1) {
-						LCD.drawString("Bad motor port: " + port, 0, 4);
-						//break;
-					}
 					try {
 						server.motors[portIndex].actionQueue.add(cmd);
 					} catch(IllegalStateException e) {
@@ -79,8 +85,15 @@ public class LocalServer {
 				LCD.drawString("Unsupported device: " + dev, 0, 4);
 			}
 		}
+		welcomeSocket.close();
 	}
 
+	private static String padString(String str) {
+		// pads string with spaces to length 100
+		String length = "100";
+		return String.format("%1$-" + length + "s", str);
+	}
+	
 	private static int getPortIndex(String port) {
 		int portIndex;
 		if (port.equals(new String("A"))) { portIndex = 0; }
